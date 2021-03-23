10ed3cf1f7860856ed0f0716b57f2fd24d4001cf
Katie <katie_lisabeth@yahoo.com>
2021-03-23 16:49:44 +0100

    create footer and home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Route path="/" exact={true} component={Home} />
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./components.css"
+
+
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="copyright">
+        Copyright © 2020 KLisabeth. All rights reserved.
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
