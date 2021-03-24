e256a861e423b6c6d5fc57bace0984eda4b58511
Katie <katie_lisabeth@yahoo.com>
2021-03-24 15:20:07 +0100

    banner

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import About from './About'
 
 function Home() {
     return (
         <div className="home">
+          <div className="banner"></div>
+            <About/>
             <Footer/>
         </div>
     )
